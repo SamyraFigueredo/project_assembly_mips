3ef1492e045d55eac8ea5d06130cc440c6558cb5
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2024-07-22 17:15:42 -0300

    verifica se o numero digitado é par ou impar.caso seja impar, retornar uma mensagem

--- enesimo_par.asm
+++ enesimo_par.asm
@@ -2,29 +2,49 @@
 
 msg1: .asciiz "Digite o enésimo número par que você gostaria de saber:  "
 msg2: .asciiz "O enésimo termo par é: "
+msg3: .asciiz "Esse número não é par"
 
 .text
 
-	# Imprimir pergunta sobre enésimo termo par
-	li $v0, 4           # Carrega o valor 4 em $v0 para realizar a chamada do sistema para imprimir uma string
-	la $a0, msg1        # Carrega o endereço da mensagem msg1 em $a0 para ser impressa
-	syscall             # Realiza a chamada do sistema para imprimir a mensagem
+main:
+    # Imprimir pergunta sobre o enésimo termo par
+    li $v0, 4           # Carrega o valor 4 em $v0 para realizar a chamada do sistema para imprimir uma string
+    la $a0, msg1        # Carrega o endereço da mensagem msg1 em $a0 para ser impressa
+    syscall             # Realiza a chamada do sistema para imprimir a mensagem
 
-	# Ler o número 
-	li $v0, 5           # Carrega o valor 5 em $v0 para realizar a chamada do sistema para ler um inteiro
-	syscall             # Realiza a chamada do sistema para ler o número digitado pelo usuário e armazenar em $v0
+    # Ler o número 
+    li $v0, 5           # Carrega o valor 5 em $v0 para realizar a chamada do sistema para ler um inteiro
+    syscall             # Realiza a chamada do sistema para ler o número digitado pelo usuário e armazenar em $v0
+    move $t0, $v0       # Move o valor digitado para $t0 para ser usado no cálculo
 
-	# Calculando o enésimo par
-	move $t0, $v0       # Move o valor digitado para $t0 para ser usado no cálculo
-	addi $t1, $t1, 2    # Adiciona 2 a $t1 para obter o primeiro número par (2)
-	mul $t2, $t1, $t0   # Multiplica $t1 (2) pelo valor digitado ($t0) para obter o enésimo termo par
+    # Verificar se o número é par
+    andi $t1, $t0, 1    # Realiza uma operação AND entre o número digitado ($t0) e 1, o resultado vai em $t1
+    bne $t1, $zero, not_even # Se $t1 não for zero, o número é ímpar; pular para not_even
 
-	# Imprimir mensagem sobre qual é o enésimo termo par
-	li $v0, 4           # Carrega o valor 4 em $v0 para realizar a chamada do sistema para imprimir uma string
-	la $a0, msg2        # Carrega o endereço da mensagem msg2 em $a0 para ser impressa
-	syscall             # Realiza a chamada do sistema para imprimir a mensagem
+    # Calculando o enésimo número par
+    li $t1, 2           # Inicializa $t1 com 2, que é o primeiro número par
+    mul $t2, $t1, $t0   # Multiplica $t1 (2) pelo valor digitado ($t0) para obter o enésimo número par
 
-	# Imprimir qual é o enésimo termo par
-	li $v0, 1           # Carrega o valor 1 em $v0 para realizar a chamada do sistema para imprimir um inteiro
-	move $a0, $t2       # Move o valor do enésimo termo par ($t2) para $a0 para ser impresso
-	syscall             # Realiza a chamada do sistema para imprimir o enésimo termo par
+    # Imprimir mensagem sobre qual é o enésimo termo par
+    li $v0, 4           # Carrega o valor 4 em $v0 para realizar a chamada do sistema para imprimir uma string
+    la $a0, msg2        # Carrega o endereço da mensagem msg2 em $a0 para ser impressa
+    syscall             # Realiza a chamada do sistema para imprimir a mensagem
+
+    # Imprimir qual é o enésimo número par
+    li $v0, 1           # Carrega o valor 1 em $v0 para realizar a chamada do sistema para imprimir um inteiro
+    move $a0, $t2       # Move o valor do enésimo número par ($t2) para $a0 para ser impresso
+    syscall             # Realiza a chamada do sistema para imprimir o enésimo número par
+
+    # Finaliza o programa
+    li $v0, 10          # Código de syscall para finalizar o programa
+    syscall             # Executa o syscall para encerrar o programa
+
+not_even:
+    # Imprimir mensagem de número não par
+    li $v0, 4           # Carrega o valor 4 em $v0 para realizar a chamada do sistema para imprimir uma string
+    la $a0, msg3        # Carrega o endereço da mensagem msg3 em $a0 para ser impressa
+    syscall             # Realiza a chamada do sistema para imprimir a mensagem
+
+    # Finaliza o programa
+    li $v0, 10          # Código de syscall para finalizar o programa
+    syscall             # Executa o syscall para encerrar o programa
